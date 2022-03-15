0da04d986e91168612ebb2335ef6bdc1ead95e89
Daoud Clarke <daoud.clarke@gmail.com>
2022-03-15 22:07:09 +0000

    Add learning to rank dataset; create a bigger dataset and upload to b2

---
+++ rankeval/ltr_dataset.py
@@ -0,0 +1,56 @@
+"""
+Generate a learn-to-rank dataset.
+"""
+import sys
+
+import pandas as pd
+from mwmbl.tinysearchengine.indexer import TinyIndex, Document
+from mwmbl.tinysearchengine.rank import Ranker
+
+from rankeval.evaluation.evaluate_ranker import DummyCompleter
+from rankeval.paths import RANKINGS_DATASET_TRAIN_PATH, LEARNING_TO_RANK_DATASET_PATH
+
+
+def run():
+    index_path = sys.argv[1]
+
+    completer = DummyCompleter()
+
+    dataset = get_dataset(completer, index_path)
+    print("Dataset", dataset)
+    pd.DataFrame(dataset).to_csv(LEARNING_TO_RANK_DATASET_PATH)
+
+
+def get_dataset(completer, index_path):
+    with TinyIndex(item_factory=Document, index_path=index_path) as tiny_index:
+        ranker = Ranker(tiny_index, completer)
+        gold_standard = pd.read_csv(RANKINGS_DATASET_TRAIN_PATH)
+        dataset = []
+        for query, rankings in gold_standard.groupby('query'):
+            print("Query", query)
+            gold_standard = set(rankings['url'].tolist())
+
+            predicted, terms = ranker.get_results(query + ' ')
+            if len(predicted) == 0:
+                continue
+
+            new_items = []
+            for item in predicted:
+                in_gold_standard = item.url in gold_standard
+                new_items.append({
+                    'in_gold_standard': in_gold_standard,
+                    'query': query,
+                    'url': item.url,
+                    'title': item.title,
+                    'extract': item.extract,
+                    'score': item.score,
+                })
+                if in_gold_standard:
+                    dataset += new_items
+                    new_items = []
+
+    return dataset
+
+
+if __name__ == '__main__':
+    run()
--- rankeval/paths.py
+++ rankeval/paths.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.parent / 'data'
 QUERIES_DATASET_PATH = DATA_DIR / 'queries.csv'
+RANKINGS_DATASET_TRAIN_PATH = DATA_DIR / 'rankings-train.csv'
 RANKINGS_DATASET_TEST_PATH = DATA_DIR / 'rankings-test.csv'
+LEARNING_TO_RANK_DATASET_PATH = DATA_DIR / 'learning-to-rank.csv'
 
 
 URLS_PATH = Path(os.environ['HOME']) / 'data' / 'tinysearch' / 'urls.sqlite3'
